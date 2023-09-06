664a84135acd69f87b6518ff72942cde1497b7ba
Wiktor Pabich <pabichwiktor@gmail.com>
2023-09-06 18:57:36 +0200

    fix: build #4

--- src/utils/money.ts
+++ src/utils/money.ts
@@ -1,3 +1,5 @@
+import type { Money } from "@/types";
+
 export const formatMoney = (money: Money) => {
 	return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(money.value);
 };
