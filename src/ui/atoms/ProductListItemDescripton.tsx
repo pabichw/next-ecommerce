2cdada8a421c42dbe9d15f90090bea460d1d51f8
Wiktor Pabich <pabichwiktor@gmail.com>
2023-09-06 21:48:09 +0200

    fix: prettier

--- src/ui/atoms/ProductListItemDescripton.tsx
+++ src/ui/atoms/ProductListItemDescripton.tsx
@@ -2,14 +2,14 @@ import type { Product } from "@/types";
 import { formatMoney } from "@/utils/money";
 
 type ProductListItemDescriptionProps = {
-  product: Product;
+	product: Product;
 };
 
 export const ProductListItemDescription = ({ product }: ProductListItemDescriptionProps) => {
-  return (
-    <div className="flex flex-col gap-4 w-full justify-between">
-      <p className="text-xs">{product.name}</p>
-      <p className="font-bold text-xs">{formatMoney(product.price)}</p>
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-4 w-full justify-between">
+			<p className="text-xs">{product.name}</p>
+			<p className="font-bold text-xs">{formatMoney(product.price)}</p>
+		</div>
+	);
 };
--- src/ui/molecules/ProductListItem.tsx
+++ src/ui/molecules/ProductListItem.tsx
@@ -3,10 +3,10 @@ import { ProductListItemCoverImage } from "@/ui/atoms/ProductListItemCoverImage"
 import { ProductListItemDescription } from "@/ui/atoms/ProductListItemDescripton";
 
 export const ProductListItem = ({ product }: { product: Product }) => {
-  return (
-    <li className="group flex flex-col gap-3 p-3 rounded-md cursor-pointer shadow shadow-slate-200 hover:shadow-xl hover:outline hover:outline-slate-200 hover:outline-1">
-      <ProductListItemCoverImage image={product.coverImage} />
-      <ProductListItemDescription product={product} />
-    </li>
-  );
+	return (
+		<li className="group flex flex-col gap-3 p-3 rounded-md cursor-pointer shadow shadow-slate-200 hover:shadow-xl hover:outline hover:outline-slate-200 hover:outline-1">
+			<ProductListItemCoverImage image={product.coverImage} />
+			<ProductListItemDescription product={product} />
+		</li>
+	);
 };
