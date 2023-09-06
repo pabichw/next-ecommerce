efb3f48b18a2ca0135b57ad4d0ba625272170b61
Wiktor Pabich <pabichwiktor@gmail.com>
2023-09-06 21:40:46 +0200

    fix: product tile styling

--- src/ui/molecules/ProductListItem.tsx
+++ src/ui/molecules/ProductListItem.tsx
@@ -3,10 +3,10 @@ import { ProductListItemCoverImage } from "@/ui/atoms/ProductListItemCoverImage"
 import { ProductListItemDescription } from "@/ui/atoms/ProductListItemDescripton";
 
 export const ProductListItem = ({ product }: { product: Product }) => {
-	return (
-		<li className="group flex flex-col gap-1 p-3 rounded-md transition ease-in-out transition-shadow shadow shadow-slate-200 outline outline-slate-200 hover:shadow-xl hover:outline-1 cursor-pointer">
-			<ProductListItemCoverImage image={product.coverImage} />
-			<ProductListItemDescription product={product} />
-		</li>
-	);
+  return (
+    <li className="group flex flex-col gap-3 p-3 rounded-md cursor-pointer shadow shadow-slate-200 hover:shadow-xl hover:outline hover:outline-slate-200 hover:outline-1">
+      <ProductListItemCoverImage image={product.coverImage} />
+      <ProductListItemDescription product={product} />
+    </li>
+  );
 };
