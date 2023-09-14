b50091aeb5a26b2a67520d420b815e787af5534c
Wiktor Pabich <pabichwiktor@gmail.com>
2023-09-14 15:54:07 +0200

    fix: lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import Navbar from "@/ui/organisms/Navbar";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "@/ui/organisms/Navbar";
 
 import "./globals.css";
 
