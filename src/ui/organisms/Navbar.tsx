6189a1e9fbd57c96e2c0f47b053a334d56965f0c
Wiktor Pabich <pabichwiktor@gmail.com>
2024-02-04 13:14:01 +0100

    chore: linting

--- src/ui/organisms/Navbar.tsx
+++ src/ui/organisms/Navbar.tsx
@@ -21,50 +21,52 @@ function Navbar({ categories }: NavbarProps) {
 	const linkTextCls = `hidden md:inline max-w-full overflow-x-hidden transition transition-[max-width]`;
 
 	return (
-		<div className="column-wrapper px-4 py-2 fixed top-0 z-10 bg-white shadow w-full flex align-center justify-between gap-8 transition-[padding]">
-			<nav role="navigation" className="mx-auto flex items-center justify-between w-full">
-				<div className={`scale-100 grayscale-0 transition-[transform,filter]`}>
-					<Logo />
-				</div>
-				<div className="flex gap-8 items-center">
-					<ActiveLink className={linkCls} activeClassName={activeLinkCls} href="/">
-						<HomeIcon className={iconCls} size={17} />
-						<span className={linkTextCls}>Home</span>
-					</ActiveLink>
-					<ActiveLink
-						className={linkCls}
-						activeClassName={activeLinkCls}
-						href={"/products" as Route}
-						exact={false}
-					>
-						<ListChecks className={iconCls} size={17} />
-						<span className={linkTextCls}>All</span>
-					</ActiveLink>
-					{categories?.map((category) => (
+		<div className="w-full fixed top-0 z-10 bg-white shadow">
+			<div className="column-wrapper px-4 py-2  flex align-center justify-between gap-8 transition-[padding]">
+				<nav role="navigation" className="mx-auto flex items-center justify-between w-full">
+					<div className={`scale-100 grayscale-0 transition-[transform,filter]`}>
+						<Logo />
+					</div>
+					<div className="flex gap-8 items-center">
+						<ActiveLink className={linkCls} activeClassName={activeLinkCls} href="/">
+							<HomeIcon className={iconCls} size={17} />
+							<span className={linkTextCls}>Home</span>
+						</ActiveLink>
 						<ActiveLink
-							key={`category-link-${category.label}`}
 							className={linkCls}
 							activeClassName={activeLinkCls}
-							href={category.url as Route}
+							href={"/products" as Route}
 							exact={false}
 						>
-							<LayersIcon className={iconCls} size={17} />
-							<span className={linkTextCls}>{category.label}</span>
+							<ListChecks className={iconCls} size={17} />
+							<span className={linkTextCls}>All</span>
 						</ActiveLink>
-					))}
+						{categories?.map((category) => (
+							<ActiveLink
+								key={`category-link-${category.label}`}
+								className={linkCls}
+								activeClassName={activeLinkCls}
+								href={category.url as Route}
+								exact={false}
+							>
+								<LayersIcon className={iconCls} size={17} />
+								<span className={linkTextCls}>{category.label}</span>
+							</ActiveLink>
+						))}
+					</div>
+				</nav>
+				<div className="flex items-center justify-center mx-3">
+					<SearchInput />
 				</div>
-			</nav>
-			<div className="flex items-center justify-center mx-3">
-				<SearchInput />
-			</div>
-			<div className="flex gap-5 items-center justify-between ">
-				<SignedIn>
-					<Link href="/orders" className="text-neutral-400 text-xs leading-3 hover:underline">
-						Your orders
-					</Link>
-				</SignedIn>
-				<Auth />
-				<Minicart />
+				<div className="flex gap-5 items-center justify-between ">
+					<SignedIn>
+						<Link href="/orders" className="text-neutral-400 text-xs leading-3 hover:underline">
+							Your orders
+						</Link>
+					</SignedIn>
+					<Auth />
+					<Minicart />
+				</div>
 			</div>
 		</div>
 	);
